75e858b7c96b460f106ba4a1eba5db2168be9f23
Mustafa Maher <mustafa.maher@elev.ga.ntig.se>
2024-02-14 10:58:18 +0100

    Tweaked interface

--- src/App.java
+++ src/App.java
@@ -23,6 +23,7 @@ public class App {
             meny();
             System.out.println("Välj ditt val 1 eller 2");
             val = tb.nextInt();
+            tb.nextLine();
             if (val==1) 
             {
                 //spellogik   
@@ -33,7 +34,6 @@ public class App {
                 
 
                 while (health>0 && health2>0) {
-                    System.out.println("Nytt varv");
                     int spelar_tal = tärning.nextInt(6);
                     Thread.sleep(100);
                     int dator_tal = tärning.nextInt(6);
@@ -41,14 +41,18 @@ public class App {
                             {
                                  int damage = tärning.nextInt(14)+7;
                                  health-=damage;
-                                System.out.println("You have been hit! Health remaining:"+(health));
+                                System.out.println("You have been hit! Health remaining: "+(health));
+                                System.out.println("Enemy combatant has "+health2+" health remaining.");
+                                System.out.println("Press enter to keep fighting.");
                                 tb.nextLine();
                             }
                         if (spelar_tal>dator_tal) 
                             {
-                                int damage2 = tärning.nextInt(14)+7;
+                                int damage2 = tärning.nextInt(7)+7;
                                 health2-=damage2;
-                                System.out.println("Successfull hit! Enemy health remaining:"+(health2));
+                                System.out.println("Successfull hit! Enemy health remaining: "+(health2));
+                                System.out.println("You have "+health+" health remaining.");
+                                System.out.println("Press enter to keep fighting.");
                                 tb.nextLine();
                             }
 
